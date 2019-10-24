45a8b1661dd396fa6999e56d21d931d15acce95d
Josh <thejoshcooter@gmail.com>
2019-10-23 20:36:08 -0500

    pg stuff

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,6 @@
 // config db connection
 const knex = require('knex');
-const config = require('../knexfile').development;
+const config = require('../knexfile');
+const environment = process.env.NODE_ENV || 'development'
 
-module.exports = knex(config);
+module.exports = knex(config[environment]);
