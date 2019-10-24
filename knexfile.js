00c03e28c8997741ba2ffe1a0bdc45142c5f21bc
Josh <thejoshcooter@gmail.com>
2019-10-23 21:06:09 -0500

    nuclear

--- knexfile.js
+++ knexfile.js
@@ -21,35 +21,35 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  }
+//   production: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       directory: './data/migrations'
+//     },
+//     seeds: {
+//       directory: './data/seeds'
+//     }
+//   }
 
-};
+// };
